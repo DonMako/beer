cd5dbea5513ff4c5eece69a534682f10131ccd40
Don Mako <lucas0203@orange.fr>
2023-02-26 21:57:55 +0100

    Correctifs légers

--- DataLayer/DAO/db_connexion.py
+++ DataLayer/DAO/db_connexion.py
@@ -3,9 +3,7 @@ import DataLayer.DAO.interface_factory as factory
 from utils.singleton import Singleton
 
 
-
 class DBConnexion(metaclass=Singleton):
-
     def __init__(self):
         self.__interface = factory.InterfaceFactory().get_interface("Connexion")
         self.__connexion = self.__interface.ouvrir_connexion(os.environ["BIERE_HOST"],
@@ -15,10 +13,10 @@ class DBConnexion(metaclass=Singleton):
                                                              os.environ["BIERE_PASSWORD"])
         if self.__connexion is None:
             raise ConnectionError
-
+        
     @property
     def connexion(self):
         return self.__connexion
-
+    
     def __del__(self):
         self.__interface.close_connexion(self.__connexion)
