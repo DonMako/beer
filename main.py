eadd802a46d1f00165e489c8ac7721653643b519
Don Mako <lucas0203@orange.fr>
2023-02-26 21:50:23 +0100

    Finalisation du fichier main.py

--- main.py
+++ main.py
@@ -5,23 +5,19 @@ import dotenv
 
 
 if __name__ == '__main__':
+    
     base_path = Path(__file__).parent
+    
     if Path(base_path / "./.env").is_file():
         dotenv_file = (base_path / "./.env").resolve()
         dotenv.load_dotenv(dotenv_file, override=True)
         vue_actuelle = StartView()
     else:
         vue_actuelle = SetupView(base_path)
-    banniere_path = (base_path / "./assets/banniere.txt").resolve()
-    with open(banniere_path, 'r', encoding="utf-8") as asset:
-        print(asset.read())
-    bordure_path = (base_path / "./assets/bordure.txt").resolve()
+    
+    border_path = (base_path / "./others/border.txt").resolve()
     while vue_actuelle:
-        with open(bordure_path, 'r', encoding="utf-8") as border:
+        with open(border_path, 'r', encoding="utf-8") as border:
             print(border.read())
             vue_actuelle.display_info()
             vue_actuelle = vue_actuelle.make_choice()
-
-    terminator_path = (base_path / "./assets/terminator.txt").resolve()
-    with open(terminator_path, 'r', encoding="utf-8") as arnold:
-        print(arnold.read())
