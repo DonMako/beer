7353211f006a9fee4ae6f87907f0f71196ca565c
Don Mako <lucas0203@orange.fr>
2023-02-26 22:35:23 +0100

    Ajout des BusinessObjects

---
+++ BusinessLayer/BusinessObjects/user.py
@@ -0,0 +1,13 @@
+import attr
+
+
+@attr.s
+class User(object):
+    prenom: str = attr.ib(converter=str, on_setattr=attr.setters.convert)
+    nom: str = attr.ib(converter=str, on_setattr=attr.setters.convert)
+    quotite: float = attr.ib(converter=float, on_setattr=attr.setters.convert)
+    agent_id: int = attr.ib(default=None, converter=attr.converters.optional(int), on_setattr=attr.setters.frozen)
+
+    def as_dict(self) -> dict:
+        data = {"identifiant_agent": self.agent_id, "quotite": self.quotite, "prenom": self.prenom, "nom": self.nom}
+        return
