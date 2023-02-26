6a924336189a041f04b494a58a972027ffb575aa
Don Mako <lucas0203@orange.fr>
2023-02-27 00:50:03 +0100

    Modification des deux bases SQL

--- DataLayer/sql/sqlite.sql
+++ DataLayer/sql/sqlite.sql
@@ -1,20 +1,20 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
-    surname VARCHAR(50) NOT NULL,
-    name_user VARCHAR(100) NOT NULL
-    id_user SERIAL PRIMARY KEY,
+    id_user CHAR(128) NOT NULL,
     password_user CHAR(128) NOT NULL,
-    favorite_type_beer float8 NOT NULL,
+    favorite_type_beer CHAR(128) NOT NULL,
+    budget_user float8 NOT NULL,
 );
 
 DROP TABLE IF EXISTS beers;
 CREATE TABLE IF NOT EXISTS beers (
     name_beer VARCHAR(50) NOT NULL,
-    price,
+    type_beer CHAR(128) NOT NULL,
+    price_beer float8 NOT NULL,
 );
 
-DROP TABLE IF EXISTS bars;
-CREATE TABLE IF NOT EXISTS bars (
-    name_bar VARCHAR(50) NOT NULL,
-    localisation,
+DROP TABLE IF EXISTS pubs;
+CREATE TABLE IF NOT EXISTS pubs (
+    name_pub VARCHAR(50) NOT NULL,
+    localisation_pub VARCHAR(50) NOT NULL,
 );
