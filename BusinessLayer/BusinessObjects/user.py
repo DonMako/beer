48f21a93b7ab84e0db0a00b437ebf0e04197174d
Don Mako <lucas0203@orange.fr>
2023-02-27 11:29:37 +0100

    Ajout du mail dans la BusinessLayer

--- BusinessLayer/BusinessObjects/user.py
+++ BusinessLayer/BusinessObjects/user.py
@@ -1,9 +1,10 @@
 class User(object):
     id: str
+    mail: str
     password: str
     favorite_beer_flavor: str
     budget: float
 
     def as_dict(self) -> dict:
-        data = {"id": self.id, "password": self.password, "favorite_beer_flavor": self.favorite_beer_flavor, "budget": self.budget}
+        data = {"id": self.id, "mail": self.mail, "password": self.password, "favorite_beer_flavor": self.favorite_beer_flavor, "budget": self.budget}
         return
--- BusinessLayer/LocalServices/user_service.py
+++ BusinessLayer/LocalServices/user_service.py
@@ -5,8 +5,8 @@ from utils.singleton import Singleton
 
 class UserService(metaclass=Singleton):
     @staticmethod
-    def create_user(id: str, password: str, favorite_beer_flavor: str, budget: float) -> bool:
-        data_user = {'id': id, 'password': password, 'favorite_beer_flavor': favorite_beer_flavor, 'budget': budget}
+    def create_user(id: str, mail: str, password: str, favorite_beer_flavor: str, budget: float) -> bool:
+        data_user = {'id': id, 'mail': mail, 'password': password, 'favorite_beer_flavor': favorite_beer_flavor, 'budget': budget}
         new_user = User.from_dict(data_user)
         return DAOUser().create_user(new_user, id, password)
 
