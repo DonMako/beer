2b189c299a16576d101c4b6ee0dd57184b2599e5
Don Mako <lucas0203@orange.fr>
2023-02-27 14:24:46 +0100

    écriture d'une méthode oubliée dans le BusinessObject User

--- BusinessLayer/BusinessObjects/user.py
+++ BusinessLayer/BusinessObjects/user.py
@@ -9,3 +9,9 @@ class User(object):
         data = {"id_user": self.id_user, "mail_user": self.mail_user, "password_user": self.password_user,
                 "favorite_beer_flavor": self.favorite_beer_flavor, "budget_user": self.budget_user}
         return data
+    
+    @staticmethod
+    def from_dict(data: dict):
+        user = User(data["id_user"], data["mail_user"], data["password_user"],
+                    data["favorite_beer_flavor"], data["budget_user"])
+        return user
