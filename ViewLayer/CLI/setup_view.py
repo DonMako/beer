f96813cb50afa7715971c47a02ccf8202961fc9b
Don Mako <lucas0203@orange.fr>
2023-02-27 12:31:01 +0100

    Correctifs des imports

--- ViewLayer/CLI/setup_view.py
+++ ViewLayer/CLI/setup_view.py
@@ -12,20 +12,20 @@ class SetupView(AbstractView):
         self.__base_path = base_path
         self.__first_try = True
         self.__choix_installation = None
-        self.__questions = [{'type': 'list', 'name': 'new_installation', 'message': 'Que souhaitez-vous faire ?',
+        self.__questions = [{'type': 'list', 'name': 'new_installation', 'message': 'What do you want to do ?',
                              'choices': ["1) Create a new BIERE installation","2) Connect to an existing BIERE installation"],
                              'default': '1', 'filter': self.__install_filter, 'when': lambda ans: self.__first_try},
                             {'type': 'list', 'name': 'engine', 'message': 'The database engine to use :',
                              'choices': ["PostgreSQL", "SQLite"], 'default': 'PostgreSQL'},
-                            {'type': 'input', 'name': 'host', 'message': "Quel est le chemin/l'hôte de la base de données ?"},
-                            {'type': 'input', 'name': 'port', 'message': 'Quel est le port de connexion ?', 
+                            {'type': 'input', 'name': 'host', 'message': "What is the database path/host?"},
+                            {'type': 'input', 'name': 'port', 'message': 'What is the connection port?', 
                              'when': lambda ans: ans['engine'] == 'PostgreSQL'},
                             {'type': 'input', 'name': 'database', 'message': "Database's name :", 
                              'when': lambda ans: ans['engine'] == 'PostgreSQL'},
-                            {'type': 'input', 'name': 'user', 'message': "User's name to connect to the database :"
+                            {'type': 'input', 'name': 'id_user', 'message': "User's id to connect to the database :"
                                                                          "\n(must have the CREATE/SELECT/INSERT/UPDATE/DELETE privileges)",
                              'when': lambda ans: ans['engine'] == 'PostgreSQL'},
-                            {'type': 'password', 'name': 'password', 'message': 'Password to connect to the database :',
+                            {'type': 'password', 'name': 'password_user', 'message': 'Password to connect to the database :',
                              'when': lambda ans: ans['engine'] == 'PostgreSQL'},
                             ]
 
@@ -54,10 +54,10 @@ class SetupView(AbstractView):
             dotenv.set_key(dotenv_file, "BIERE_PORT", str(answers.get('port', "")))
             os.environ["BIERE_DATABASE"] = str(answers.get('database', ""))
             dotenv.set_key(dotenv_file, "BIERE_DATABASE", str(answers.get('database', "")))
-            os.environ["BIERE_USER"] = str(answers.get('user', ""))
-            dotenv.set_key(dotenv_file, "BIERE_USER", str(answers.get('user', "")))
-            os.environ["BIERE_PASSWORD"] = str(answers.get('password', ""))
-            dotenv.set_key(dotenv_file, "BIERE_PASSWORD", str(answers.get('password', "")))
+            os.environ["BIERE_USER"] = str(answers.get('id_user', ""))
+            dotenv.set_key(dotenv_file, "BIERE_USER", str(answers.get('id_user', "")))
+            os.environ["BIERE_PASSWORD"] = str(answers.get('password_user', ""))
+            dotenv.set_key(dotenv_file, "BIERE_PASSWORD", str(answers.get('password_user', "")))
             try:
                 DBConnexion().connexion.cursor().close()
                 connexion_ok = True
