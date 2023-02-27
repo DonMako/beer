4ce917cda327cdaf8717c6c33edfb8e404469b8c
Don Mako <lucas0203@orange.fr>
2023-02-27 12:04:37 +0100

    Changements mineurs

--- ViewLayer/CLI/connexion_view.py
+++ ViewLayer/CLI/connexion_view.py
@@ -1,20 +1,20 @@
-from PyInquirer import prompt
+from BusinessLayer.BusinessObjects.user import User
 from BusinessLayer.LocalServices.session_service import SessionService
+from PyInquirer import prompt
 from ViewLayer.CLI.abstract_view import AbstractView
 from ViewLayer.CLI.menu import MenuView
 from ViewLayer.CLI.session import Session
-from BusinessLayer.BusinessObjects.user import User
 
 
 class ConnexionView(AbstractView):
     def __init__(self) -> None:
-        self.__questions = [{'type': 'input', 'name': 'id', 'message': 'Username :'},
-                            {'type': 'password', 'name': 'password', 'message': 'Password :'}]
+        self.__questions = [{'type': 'input', 'name': 'id_user', 'message': 'Username :'},
+                            {'type': 'password', 'name': 'password_user', 'message': 'Password :'}]
         self.__error = "Incorrect username and/or password."
 
     def make_choice(self):
         answers = prompt(self.__questions)
-        user = SessionService().open_session(answers['id'], answers['password'])
+        user = SessionService().open_session(answers['id_user'], answers['password_user'])
         if not (isinstance(user, User)):
             print(self.__error)
             return ConnexionView()
