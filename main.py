7e242e2c709ad811b868cf47ad15ec433a94d02a
Don Mako <lucas0203@orange.fr>
2023-02-27 12:01:54 +0100

    Réorganisation du fichier main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from ViewLayer.CLI.start_view import StartView
-from ViewLayer.CLI.setup_view import SetupView
-from pathlib import Path
 import dotenv
+from pathlib import Path
+from ViewLayer.CLI.setup_view import SetupView
+from ViewLayer.CLI.start_view import StartView
 
 
 if __name__ == '__main__':
@@ -9,12 +9,12 @@ if __name__ == '__main__':
     if Path(base_path / "./.env").is_file():
         dotenv_file = (base_path / "./.env").resolve()
         dotenv.load_dotenv(dotenv_file, override=True)
-        vue_actuelle = StartView()
+        actual_view = StartView()
     else:
-        vue_actuelle = SetupView(base_path)
+        actual_view = SetupView(base_path)
     border_path = (base_path / "./others/border.txt").resolve()
-    while vue_actuelle:
+    while actual_view:
         with open(border_path, 'r', encoding="utf-8") as border:
             print(border.read())
-            vue_actuelle.display_info()
-            vue_actuelle = vue_actuelle.make_choice()
+            actual_view.display_info()
+            actual_view = actual_view.make_choice()
