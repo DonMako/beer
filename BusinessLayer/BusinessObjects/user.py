6b4039e505de960323bc73aa85e6edce41827d36
Don Mako <lucas0203@orange.fr>
2023-02-27 17:50:42 +0100

    Modification de la classe User: ajout de la méthode from_dict

--- BusinessLayer/BusinessObjects/user.py
+++ BusinessLayer/BusinessObjects/user.py
@@ -14,3 +14,8 @@ class User(object):
         data = {"id_user": self.id_user, "email_user": self.email_user, "password_user": self.password_user,
                 "favorite_beer_flavor": self.favorite_beer_flavor, "budget_user": self.budget_user}
         return data
+    
+    @staticmethod
+    def from_dict(data: dict):
+        user = User(data["id_user"], data["email_user"], data["password_user"], data["favorite_beer_flavor"], data["budget_user"])
+        return user
