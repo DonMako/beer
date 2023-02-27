9a95031ec79f41d620f40d42ad789a1da0bf4bc3
Don Mako <lucas0203@orange.fr>
2023-02-27 12:09:50 +0100

    Ajout du mail + réorganisation de la vue NewUser

--- ViewLayer/CLI/new_user_view.py
+++ ViewLayer/CLI/new_user_view.py
@@ -1,6 +1,6 @@
-from PyInquirer import prompt
 from BusinessLayer.LocalServices.session_service import SessionService
 from BusinessLayer.LocalServices.user_service import UserService
+from PyInquirer import prompt
 from ViewLayer.CLI.abstract_view import AbstractView
 from ViewLayer.CLI.menu import MenuView
 from ViewLayer.CLI.session import Session
@@ -8,16 +8,18 @@ from ViewLayer.CLI.session import Session
 
 class NewUserView(AbstractView):
     def __init__(self) -> None:
-        self.__questions = [{'type': 'input', 'name': 'id', 'message': 'Id :'},
-                            {'type': 'password', 'name': 'password', 'message': 'Password :'},
-                            {'type': 'input', 'name': 'favorite_beer_flavor', 'message': "Favorite beer flavor :"},
-                            {'type': 'input', 'name': 'budget', 'message': "Budget :", 'filter': float}]
+        self.__questions = [{'type': 'input', 'name': 'id_user', 'message': 'Enter an id :'},
+                            {'type': 'input', 'name': 'mail_user', 'message': 'Enter a mail :'},
+                            {'type': 'password', 'name': 'password_user', 'message': 'Enter a password :'},
+                            {'type': 'input', 'name': 'favorite_beer_flavor', 'message': "Enter your favorite beer flavor :"},
+                            {'type': 'input', 'name': 'budget_user', 'message': "Enter your budget :", 'filter': float}]
 
     def make_choice(self):
         answers = prompt(self.__questions)
-        succes = UserService().create_user(answers['id'], answers['password'], answers['favorite_beer_flavor'], answers['budget'])
+        succes = UserService().create_user(answers['id_user'], answers['mail_user'], answers['password_user'], 
+                                           answers['favorite_beer_flavor'], answers['budget_user'])
         if not(succes):
             print("Error")
             return NewUserView()
-        Session().user = SessionService().open_session(answers['id'], answers['password'])
+        Session().user = SessionService().open_session(answers['id_user'], answers['password_user'])
         return MenuView()
