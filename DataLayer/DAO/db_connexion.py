cc7c692ca2bd4fd72c9a5d98eccc33a1086bc961
Don Mako <lucas0203@orange.fr>
2023-02-27 11:37:18 +0100

    Correctif du db_connexion

--- DataLayer/DAO/db_connexion.py
+++ DataLayer/DAO/db_connexion.py
@@ -1,5 +1,5 @@
-import os
 import DataLayer.DAO.interface_factory as factory
+import os
 from utils.singleton import Singleton
 
 
@@ -7,10 +7,10 @@ class DBConnexion(metaclass=Singleton):
     def __init__(self):
         self.__interface = factory.InterfaceFactory().get_interface("Connexion")
         self.__connexion = self.__interface.open_connexion(os.environ["BIERE_HOST"],
-                                                             os.environ["BIERE_PORT"],
-                                                             os.environ["BIERE_DATABASE"],
-                                                             os.environ["BIERE_USER"],
-                                                             os.environ["BIERE_PASSWORD"])
+                                                           os.environ["BIERE_PORT"],
+                                                           os.environ["BIERE_DATABASE"],
+                                                           os.environ["BIERE_USER"],
+                                                           os.environ["BIERE_PASSWORD"])
         if self.__connexion is None:
             raise ConnectionError
         
