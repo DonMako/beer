7c3630681f229f97459b0e6a1093c709c8611e52
Don Mako <lucas0203@orange.fr>
2023-02-27 11:42:50 +0100

    Simplification de l'étape WHEN + ajout du mail

--- tests/test_BusinessLayer/test_BusinessObjects/test_user.py
+++ tests/test_BusinessLayer/test_BusinessObjects/test_user.py
@@ -1,41 +1,41 @@
-from unittest import TestCase
 from BusinessLayer.BusinessObjects.user import User
+from unittest import TestCase
 
 
 class TestUser(TestCase):
 
     def test_create_user(self):
         # GIVEN
-        user_id = 'toto'
-        user_password = '9520df6412dfdf'
-        user_favorite_beer_flavor = 'bière blonde'
-        user_budget = 90.00
+        id_example = 'toto'
+        mail_example = 'toto@mail.com'
+        password_example = '9520df6412dfdf'
+        favorite_beer_flavor_example = 'bière blonde'
+        budget_example = 90.00
         # WHEN
-        user = User(user_id, user_password, user_favorite_beer_flavor, user_budget)
-        id = user.id
-        password = user.password
-        favorite_beer_flavor = user.cp
-        budget = user.ville
+        user = User(id_example, mail_example, password_example, favorite_beer_flavor_example, budget_example)
         # THEN
         self.assertIsInstance(user, User)
-        self.assertEqual('toto', id)
-        self.assertEqual('9520df6412dfdf', password)
-        self.assertEqual('bière blonde', favorite_beer_flavor)
-        self.assertEqual(90.00, budget)
+        self.assertEqual('toto', user.id_user)
+        self.assertEqual('toto@mail.com', user.mail_user)
+        self.assertEqual('9520df6412dfdf', user.password_user)
+        self.assertEqual('bière blonde', user.favorite_beer_flavor)
+        self.assertEqual(90.00, user.budget_user)
 
     def test_equal_users(self):
         # GIVEN
         user1_id = 'toto'
+        user1_mail = 'toto@mail.com'
         user1_password = "df54eq65g4zq6ffjzefz"
         user1_favorite_beer_flavor = 'bière ambrée'
         user1_budget = 2.50
         user2_id = 'tata'
+        user2_mail = 'tata@mail.com'
         user2_password = "zfhi5557"
         user2_favorite_beer_flavor = 'triple'
         user2_budget = 37.64
         # WHEN
-        user1 = User(user1_id, user1_password, user1_favorite_beer_flavor, user1_budget)
-        user2 = User(user2_id, user2_password, user2_favorite_beer_flavor, user2_budget)
+        user1 = User(user1_id, user1_mail, user1_password, user1_favorite_beer_flavor, user1_budget)
+        user2 = User(user2_id, user2_mail, user2_password, user2_favorite_beer_flavor, user2_budget)
         # THEN
-        self.assertTrue(User('toto', "df54eq65g4zq6ffjzefz", 'bière ambrée', 2.50) == user1)
+        self.assertTrue(User('toto', 'toto@mail.com', "df54eq65g4zq6ffjzefz", 'bière ambrée', 2.50) == user1)
         self.assertFalse(user1 == user2)
