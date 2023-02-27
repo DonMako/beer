f72897d22b03029570564e8b88e44b016ad33ba3
Don Mako <lucas0203@orange.fr>
2023-02-27 12:34:48 +0100

    Ajout de l'adresse mail

--- ViewLayer/CLI/modify_user_view.py
+++ ViewLayer/CLI/modify_user_view.py
@@ -13,36 +13,41 @@ class ModifyUserView(AbstractView):
             self.__user = Session().user
         else:
             self.__user = user
-        self.__main_prompt = [{'type': 'list', 'name': 'choice',
+        self.__main_prompt = [{'type': 'list', 'name': 'choices',
                                'message': 'What do you want to modify ?',
-                               'choices': ['P) Password', "F) Favourite beer's flavour", 'B) Budget', 'D) Delete account']}]
-        self.__main_prompt[0]['choices'].append('M) Menu')
-        self.__continue_prompt = [{'type': 'list', 'name': 'choice', 'message': 'Modify something else ?',
+                               'choices': ['M) Mailing adress', 'P) Password', "F) Favourite beer's flavour", 'B) Budget', 'D) Delete account']}]
+        self.__main_prompt[0]['choices'].append('H) Menu')
+        self.__continue_prompt = [{'type': 'list', 'name': 'choices', 'message': 'Modify something else ?',
                                    'choices': ['Y) Yes', 'N) No']}]
 
     def make_choice(self):
         modify = True
         while modify:
             answers0 = prompt(self.__main_prompt)
-            if str.upper(answers0['choices'][0]) == "M":
+            if "H" in str.upper(answers0['choices'][0]):
                 modify = False
             else:
-                if str.upper(answers0['choices'][0]) == "P":
-                    prompt_password = [{'type': 'input', 'name': 'password', 'message': "New password :"}]
+                if "M" in str.upper(answers0['choices'][0]):
+                    prompt_mail = [{'type': 'input', 'name': 'mail_user', 'message': "New mailing adress :"}]
+                    answer = prompt(prompt_mail)
+                    self.__user.mail_user = answer['mail_user']
+                    succes = UserService().modify_user(self.__user)
+                elif "P" in str.upper(answers0['choices'][0]):
+                    prompt_password = [{'type': 'input', 'name': 'password_user', 'message': "New password :"}]
                     answer = prompt(prompt_password)
                     self.__user.password_user = answer['password']
                     succes = UserService().modify_user(self.__user)
-                elif str.upper(answers0['choices'][0]) == "F":
+                elif "F" in str.upper(answers0['choices'][0]):
                     prompt_favorite_beer_flavor = [{'type': 'input', 'name': 'favorite_beer_flavor', 'message': "New favorite beer flavor :"}]
                     answer = prompt(prompt_favorite_beer_flavor)
                     self.__user.favorite_beer_flavor = answer['favorite_beer_flavor']
                     succes = UserService().modify_user(self.__user)
-                elif str.upper(answers0['choices'][0]) == "B":
-                    prompt_budget = [{'type': 'input', 'name': 'budget', 'message': "New budget :",'filter': float}]
+                elif "B" in str.upper(answers0['choices'][0]):
+                    prompt_budget = [{'type': 'input', 'name': 'budget_user', 'message': "New budget :",'filter': float}]
                     answer = prompt(prompt_budget)
-                    self.__user.budget_user = answer['budget']
+                    self.__user.budget_user = answer['budget_user']
                     succes = UserService().modify_user(self.__user)
-                elif str.upper(answers0['choices'][0]) == "D":
+                elif "D" in str.upper(answers0['choices'][0]):
                     prompt_verif = [{'type': 'input', 'name': 'verif', 'message': "You are about to delete your account. Do you confirm to do this action ?"}]
                     answer = prompt(prompt_verif)
                     if answer:
