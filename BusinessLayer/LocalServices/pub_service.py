33d6fdff65846dabd4e798ec08e85bdba4310504
Don Mako <lucas0203@orange.fr>
2023-02-28 22:25:27 +0100

    Ajout de la méthode 'get_adress_pub' dans les couches métiers et données

--- BusinessLayer/LocalServices/pub_service.py
+++ BusinessLayer/LocalServices/pub_service.py
@@ -7,9 +7,13 @@ from utils.singleton import Singleton
 class PubService(metaclass=Singleton):
     
     @staticmethod
-    def get_pubs_localisation(localisation: str) -> List:
-        return dao_pub.DAOPub().get_pubs_localisation(localisation)
+    def get_pubs_city(city: str) -> List:
+        return dao_pub.DAOPub().get_pubs_city(city)
     
     @staticmethod
     def get_name_pub(pub: Pub) -> str:
         return dao_pub.DAOPub().get_name_pub(pub)
+    
+    @staticmethod
+    def get_adress_pub(pub: Pub) -> str:
+        return dao_pub.DAOPub().get_adress_pub(pub)
--- DataLayer/DAO/interface_pub.py
+++ DataLayer/DAO/interface_pub.py
@@ -6,9 +6,13 @@ from typing import List
 class InterfacePub(ABC):
     
     @abstractmethod
-    def get_pubs_localisation(self, localisation: str) -> List:
+    def get_pubs_city(self, city: str) -> List:
         raise NotImplementedError
     
     @abstractmethod
-    def get_name_pub(self, pub: Pub) -> List:
+    def get_name_pub(self, pub: Pub) -> str:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_adress_pub(self, pub: Pub) -> str:
         raise NotImplementedError
