a3718c7b569f2fb85bded94ed5c252610d16c733
ensai <evain.manon@orange.fr>
2023-03-01 11:52:12 +0100

    Changements instanciation table

--- DataLayer/sql/postgresql.sql
+++ DataLayer/sql/postgresql.sql
@@ -1,28 +1,28 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
-    id_user CHAR(128) NOT NULL,
+    id_user CHAR(128) PRIMARY KEY NOT NULL,
     password_user CHAR(128) NOT NULL,
     favorite_type_beer CHAR(128) NOT NULL,
-    budget_user float8 NOT NULL,
+    budget_user float NOT NULL,
 );
 
 DROP TABLE IF EXISTS beers;
 CREATE TABLE IF NOT EXISTS beers (
-    name_beer VARCHAR(50) NOT NULL,
+    name_beer VARCHAR(50) PRIMARY KEY NOT NULL,
     type_beer CHAR(128) NOT NULL,
-);
-
-DROP TABLE IF EXISTS menu;
-CREATE TABLE IF NOT EXISTS menu (
-    name_pub VARCHAR(50) NOT NULL,
-    name_beer VARCHAR(50) NOT NULL,
-    price_beer float8 NOT NULL,
 );
 
 DROP TABLE IF EXISTS pubs;
 CREATE TABLE IF NOT EXISTS pubs (
-    name_pub VARCHAR(50) NOT NULL,
+    name_pub VARCHAR(50) PRIMARY KEY NOT NULL,
     localisation_pub VARCHAR(50) NOT NULL,
+);
+
+DROP TABLE IF EXISTS menu;
+CREATE TABLE IF NOT EXISTS menu (
+    price_beer float NOT NULL,
+    FOREIGN KEY (name_beer) REFERENCES beers(name_beer)
+    FOREIGN KEY (name_pub) REFERENCES pubs(name_pub)
 );
 
 INSERT INTO beers (name_beer, type_beer)
@@ -40,3 +40,5 @@ VALUES
     ('1664 Blanche', 'Blanche'),
     ('Leffe', 'Blonde'), 
     ('Leffe Ruby', 'Rouge');
+
+INSERT INTO
